b648edcab8d43af26a5a76ecf56a0c5f11b70a52
Phebueno <fcbgomes@gmail.com>
2023-09-03 14:15:45 -0300

    refactor: cryptrService included in other routes

--- src/credentials/credentials.module.ts
+++ src/credentials/credentials.module.ts
@@ -3,10 +3,11 @@ import { CredentialsService } from './credentials.service';
 import { CredentialsController } from './credentials.controller';
 import { AuthModule } from '../auth/auth.module';
 import { CredentialsRepository } from './credentials.repository';
+import { CryptrService } from '../utils/cryptr.service';
 
 @Module({
   imports:[AuthModule],
   controllers: [CredentialsController],
-  providers: [CredentialsService, CredentialsRepository],
+  providers: [CredentialsService, CredentialsRepository, CryptrService],
 })
 export class CredentialsModule {}
--- src/credentials/credentials.service.ts
+++ src/credentials/credentials.service.ts
@@ -1,18 +1,24 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateCredentialDto } from './dto/create-credential.dto';
 import { User } from '@prisma/client';
 import { CredentialsRepository } from './credentials.repository';
 import Cryptr from 'cryptr';
+import { CryptrService } from '../utils/cryptr.service';
 
 @Injectable()
 export class CredentialsService {
-
   private SALT = 10;
   private ITERATIONS = 10000;
   private cryptr: Cryptr;
 
-  constructor(private readonly credentialsRepository: CredentialsRepository) {    
-  }
+  constructor(
+    private readonly credentialsRepository: CredentialsRepository,
+    private readonly cryptrService: CryptrService,
+  ) {}
 
   private getCryptrInstance(): Cryptr {
     if (!this.cryptr) {
@@ -25,7 +31,7 @@ export class CredentialsService {
   }
 
   async create(credentialDto: CreateCredentialDto, user: User) {
-    const cryptr = this.getCryptrInstance();
+    const cryptr = this.cryptrService.getCryptrInstance();
     const encryptedPass = cryptr.encrypt(credentialDto.password);
     return await this.credentialsRepository.create(
       { ...credentialDto, password: encryptedPass },
@@ -36,16 +42,19 @@ export class CredentialsService {
   async findAll(user: User) {
     const credentials = await this.credentialsRepository.findAll(user.id);
 
-    const cryptr = this.getCryptrInstance();
-    credentials.map(cred => { cred.password = cryptr.decrypt(cred.password)})
+    const cryptr = this.cryptrService.getCryptrInstance();
+    credentials.map((cred) => {
+      cred.password = cryptr.decrypt(cred.password);
+    });
     return credentials;
   }
 
   async findOne(id: number, user: User) {
     const credential = await this.credentialsRepository.findOne(id);
-    if(!credential) throw new NotFoundException("Credential not found!");
-    if(credential.userId !== user.id) throw new UnauthorizedException("Not owner of credential!")
-    
+    if (!credential) throw new NotFoundException('Credential not found!');
+    if (credential.userId !== user.id)
+      throw new UnauthorizedException('Not owner of credential!');
+
     const cryptr = this.getCryptrInstance();
     const decryptedPass = cryptr.decrypt(credential.password);
     return { ...credential, password: decryptedPass };
